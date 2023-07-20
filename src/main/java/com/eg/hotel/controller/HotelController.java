5360519ae0ab172064108d8d4105e23bf7b2736a
Enrico Albano <eaalbano@amazon.com>
2023-07-20 20:15:40 +0200

    Added API logic for filtering and sorting

--- src/main/java/com/eg/hotel/controller/HotelController.java
+++ src/main/java/com/eg/hotel/controller/HotelController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @RestController
@@ -21,7 +23,22 @@ public class HotelController {
 
     @GetMapping("/hotels")
     public List<Hotel> getHotels(@ModelAttribute SearchQuery query) {
-        // Returning database entries for now
-        return hotelDatabase.getAllHotels();
+        List<Hotel> allHotels = hotelDatabase.getAllHotels();
+        List<Hotel> filteredHotels = filterHotels(allHotels, query);
+        return sortHotels(filteredHotels);
+    }
+
+    private List<Hotel> filterHotels(List<Hotel> hotels, SearchQuery query) {
+        return hotels.stream()
+                .filter(hotel -> hotel.getLocation().getName().equalsIgnoreCase(query.getLocation()))
+                .filter(hotel -> hotel.getPrice() >= query.getPriceRange().get(0)
+                        && hotel.getPrice() <= query.getPriceRange().get(1))
+                .filter(hotel -> !hotel.isBooked(query.getCheckinDate(), query.getCheckoutDate()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Hotel> sortHotels(List<Hotel> hotels) {
+        hotels.sort(Comparator.comparing(Hotel::getAverageReview).reversed());
+        return hotels;
     }
 }
--- src/main/java/com/eg/hotel/model/Hotel.java
+++ src/main/java/com/eg/hotel/model/Hotel.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
@@ -17,4 +18,23 @@ public class Hotel {
     private int price;
     private String imageUrl;
     private List<HotelReview> reviews;
+
+    public double getAverageReview() {
+        if (reviews == null) {
+            return 0;
+        }
+
+        int sum = 0;
+        for (HotelReview hotelReview : reviews) {
+            sum += hotelReview.getRating();
+        }
+
+        return (double) sum / reviews.size();
+    }
+
+    public boolean isBooked(LocalDate checkinDate, LocalDate checkoutDate) {
+        // Logic to check if the hotel is booked for the given date range.
+        // For this example, let's assume no hotels are booked for simplicity.
+        return false;
+    }
 }
